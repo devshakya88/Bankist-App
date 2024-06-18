3a2979250e13c69d2c8079ce5fd600f136dd4724
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-06-18 09:01:08 +0530

    Navbar Fade Done

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnscrollto = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -50,10 +54,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 //Tabbed Component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
   // console.log(clicked);
@@ -77,3 +77,31 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Menu fade Animation
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+// Create a function that takes an opacity value and returns an event handler
+const createHandleHover = function (opacity) {
+  return function (e) {
+    handleHover(e, opacity);
+  };
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', createHandleHover(0.5));
+nav.addEventListener('mouseout', createHandleHover(1));
